c0e154a73b227c4c4870ce4d44f9acbfd98310b7
Aharon Campos <aharoncampos@gmail.com>
2015-05-08 16:47:29 -0500

    Saboteour: move to nodes with less neighbors
    This, in principle, would take us to unexplored nodes, thus moving
    farther away in the map.
    
    However, a more robust logic is required, to check whether the edges are
    unsurveyed before moving there.
    
    Also, need to check if there's any ongoing attack to be able to truly
    harm enemies.
    
    Signed-off-by: Aharon Campos <aharoncampos@gmail.com>

--- src/massim/javaagents/agents/SimpleSaboteurAgent.java
+++ src/massim/javaagents/agents/SimpleSaboteurAgent.java
@@ -41,7 +41,7 @@ public class SimpleSaboteurAgent extends Agent {
 		if ( act != null ) return act;
 		
 		// 3. random walking
-		act = planRandomWalk();
+		act = planWalk();
 		if ( act != null ) return act;
 
 		return MarsUtil.skipAction();
@@ -272,7 +272,7 @@ public class SimpleSaboteurAgent extends Agent {
 		return null;
 	}
 	
-	private Action planRandomWalk() {
+	private Action planWalk() {
 
 		LinkedList<LogicBelief> beliefs = getAllBeliefs("neighbor");
 		Vector<String> neighbors = new Vector<String>();
@@ -282,11 +282,20 @@ public class SimpleSaboteurAgent extends Agent {
 		
 		if ( neighbors.size() == 0 ) {
 			println("strangely I do not know any neighbors");
-			return MarsUtil.skipAction();
+			return MarsUtil.rechargeAction();
 		}
-		
-		// goto neighbors
-		Collections.shuffle(neighbors);
+                
+		// Contribute to map coverage
+                // TODO: as attackers, we may want to go where our allies are attacking others. That needs to go before this logic.
+                int amountOfNeighbors = 10; // hopefully not so small
+                String nodeWithLessNeighbors;
+                for (String neighbor : neighbors) {
+                    int amount = mapGraph.degreeOf(neighbor);
+                    if (amount < amountOfNeighbors) {
+                        amountOfNeighbors = amount;
+                        nodeWithLessNeighbors = neighbor;
+                    }
+                }
 		String neighbor = neighbors.firstElement();
 		println("I will go to " + neighbor);
 		return MarsUtil.gotoAction(neighbor);
